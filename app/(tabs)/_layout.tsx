d2c0eaf44cc66a03818c94d8faeb8a199b743f3e
Danna Pabayo <dannapabayo@gmail.com>
2026-06-22 19:51:09 +1000

    Sprint 1: Removed example app and added styling to navigation bar.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,42 @@
 import { Tabs } from 'expo-router';
-
+import {MaterialIcons} from "@expo/vector-icons";
 
 export default function TabLayout() {
     return (
-        <Tabs>
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: '#168AAD',
+                tabBarInactiveTintColor: 'grey',
+                animation: 'shift',
+                tabBarStyle: {
+                    height: 80,
+                    paddingTop: 8,
+                    paddingBottom: 8,
+                },
+                tabBarIconStyle: {
+                    marginBottom: 5,
+                },
+                tabBarLabelStyle: {
+                    fontSize: 15,
+                }
+            }}
+        >
             <Tabs.Screen
                 name={"index"}
-                options={{title: "Home"}}
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ color }) =>
+                        <MaterialIcons size={30} name={"home"} color={color} />
+                }}
             />
             <Tabs.Screen
                 name={"settings"}
-                options={{title: "Settings"}}
+                options={{
+                    title: "Settings",
+                    tabBarIcon: ({ color }) =>
+                        <MaterialIcons size={30} name="settings" color={color} />
+                }}
             />
         </Tabs>
     )
