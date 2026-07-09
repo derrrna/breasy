a0ea5a376fe9be9edad82e06f329f90ca45cb82e
Danna Pabayo <dannapabayo@gmail.com>
2026-07-09 14:55:05 +1000

    Sprint 4: Created low fidelity UI of settings screen

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -3,6 +3,7 @@ import {ScrollView, Text, View} from 'react-native';
 import RadioButton from "@/components/radioButton";
 import {SettingsContext} from "@/store/settingsContext";
 import {useContext} from "react";
+import Slider from "@react-native-community/slider";
 
 export default function Settings(){
 
@@ -11,17 +12,28 @@ export default function Settings(){
     const handleRadioPress = () => {}
 
     return (
-        <ScrollView>
-            {/* FORM TODO: make as radio button components. Use FieldGroup*/}
-            <Text>Exercise Presets</Text>
-            <View className={'flex-col'}>
-                <RadioButton optionName={"Box Breathing"} handlePress={handleRadioPress}/>
-                <RadioButton optionName={"TIPP"} handlePress={handleRadioPress}/>
-                <RadioButton optionName={"Third Option"} handlePress={handleRadioPress}/>
+        <ScrollView contentContainerClassName={"justify-center items-center px-8"}>
+
+            <Text className={"text-3xl py-6"}>Settings</Text>
+
+            <View className={'flex-col w-full'}>
+                <Text className={"text-xl my-3"}>Breathing Exercise</Text>
+                <View className={"pl-5"}>
+                    <RadioButton optionName={"Box Breathing"} handlePress={handleRadioPress}/>
+                    <RadioButton optionName={"TIPP"} handlePress={handleRadioPress}/>
+                    <RadioButton optionName={"Third Option"} handlePress={handleRadioPress}/>
+                </View>
+
             </View>
-            <Text>Custom</Text>
-            <View>
-                <Text>Inhale Count</Text>
+
+            <View className={'flex-col w-full pt-8'}>
+                <Text className={"text-xl my-3"}>Vibration Count</Text>
+                <Slider
+                    minimumValue={0}
+                    maximumValue={5}
+                    step={1}
+                    renderStepNumber={true}
+                />
             </View>
         </ScrollView>
     )
--- components/radioButton.tsx
+++ components/radioButton.tsx
@@ -7,9 +7,9 @@ interface RadioButtonProps {
 
 export default function RadioButton(props: RadioButtonProps) {
     return (
-        <View className={'flex-row items-center'}>
-            <Pressable onPress={props.handlePress} className={'bg-black w-2 h-2'}/>
-            <Text>{props.optionName}</Text>
+        <View className={'flex-row items-center py-3'}>
+            <Pressable onPress={props.handlePress} className={'bg-black w-7 h-7 rounded-full'}/>
+            <Text className={"ml-5"}>{props.optionName}</Text>
         </View>
     )
 }
