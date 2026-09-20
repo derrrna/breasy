c793d7e4e023d43043c101f6feca7ded6634d503
Danna Pabayo <dannapabayo@gmail.com>
2026-09-20 15:06:08 +1000

    Fixed reset button for paced breathing

--- components/buttons/textPillButton.tsx
+++ components/buttons/textPillButton.tsx
@@ -1,5 +1,6 @@
-import {Pressable, StyleSheet, Text} from "react-native";
+import {Pressable, Text} from "react-native";
 import React from "react";
+import Animated, {interpolateColor, useAnimatedStyle, useSharedValue, withTiming} from "react-native-reanimated";
 import colors from "@/utils/colors";
 
 interface TextPillButtonProps {
@@ -8,20 +9,24 @@ interface TextPillButtonProps {
 }
 
 export default function TextPillButton({label, onPress}: TextPillButtonProps) {
+    const colorProgress = useSharedValue(0);
+
+    const onPressIn = () => {
+        colorProgress.value = withTiming(1, {duration: 100});
+    };
+    const onPressOut = () => {
+        colorProgress.value = withTiming(0, {duration: 200});
+    };
+
+    const containerStyle = useAnimatedStyle(() => ({
+        backgroundColor: interpolateColor(colorProgress.value, [0, 1], [colors.primary, colors.primarySoft]),
+    }));
+
     return (
-        <Pressable
-            onPress={onPress}
-            style={({pressed}) => [styles.pill, {backgroundColor: pressed ? colors.primarySoft : colors.primary}]}>
-            <Text className={"text-offWhite font-interSemiBold text-sm"}>{label}</Text>
+        <Pressable onPress={onPress} onPressIn={onPressIn} onPressOut={onPressOut} style={{alignSelf: "flex-start"}}>
+            <Animated.View style={containerStyle} className={"rounded-xl px-5 py-3 items-center justify-center"}>
+                <Text className={"text-offWhite font-interSemiBold text-sm"}>{label}</Text>
+            </Animated.View>
         </Pressable>
     );
 }
-
-const styles = StyleSheet.create({
-    pill: {
-        alignSelf: "flex-start",
-        borderRadius: 999,
-        paddingHorizontal: 16,
-        paddingVertical: 8,
-    },
-});
